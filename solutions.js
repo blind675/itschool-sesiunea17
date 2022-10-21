82d3275de12054d1e3be5076a5b1ef4dcf98d405
blind675 <catalin.bora@gmail.com>
2022-10-21 10:06:58 +0300

    update function names

--- solutions.js
+++ solutions.js
@@ -3,9 +3,9 @@ let salaries = [1036, 3205, 2030, 2945];
 
 // 1.
 // Creaza o functie care primeste ca parametrul lista si
-// returneaza prima valoare dvizibila cu 5. ( o poti numi 'find' :) )
+// returneaza prima valoare dvizibila cu 5. ( o poti numi 'simpleFind' :) )
 // Hint: poti folosi break pentru a opri parcurgerea listei cand ai gasit valoarea dorita.
-function find(list) {
+function simpleFind(list) {
   let value;
   for(let i = 0; i < list.length; i++) {
     if(list[i] % 5 === 0) {
@@ -17,7 +17,7 @@ function find(list) {
   return value;
 }
 
-console.log(find(salaries));
+console.log(simpleFind(salaries));
 
 // 2.
 // Creaza o functie care primeste o valoare si returneaza true daca este intre 2500 si 3000
@@ -27,10 +27,10 @@ function valueInRange(value) {
 
 // 3.
 // Pornind de la codul functiei 'find' de mai sus creaza o functie care primeste ca parametrul lista si o functie callback
-// si returneaza prima valoare care respecta conditia testata in functia callaback ( o poti numi 'findWithCallback' )
-// Apeleaza functia 'findWithCallback' folosind functia de la punctul 2 ca si functie de callback
+// si returneaza prima valoare care respecta conditia testata in functia callaback ( o poti numi 'find' )
+// Apeleaza functia 'find' folosind functia de la punctul 2 ca si functie de callback
 
-function findWithCallback(list, callback) {
+function find(list, callback) {
   let value;
 
   for(let i = 0; i < list.length; i++) {
@@ -43,10 +43,10 @@ function findWithCallback(list, callback) {
   return value;
 }
 
-console.log(findWithCallback(salaries, valueInRange));
+console.log(find(salaries, valueInRange));
 
 // 4. 
 // Apeleaza functia de la punctul 3. cu lista (array-ul) salaries si o functie care testeaza ca o valoare e divizibila cu 10
 
-let response = findWithCallback(salaries, (value) => value % 10 === 0);
+let response = find(salaries, (value) => value % 10 === 0);
 console.log(response);
